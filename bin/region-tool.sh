a415b6253890933f832dbb0b12a7dd44f2b36d4f
rarana <rafael.arana@cloudera.com>
2017-09-14 11:00:11 +0200

    fix classpath

--- bin/region-tool.sh
+++ bin/region-tool.sh
@@ -6,6 +6,6 @@ TOOL_BIN_DIR=/root/region-tool/bin
 TOOL_LIB_DIR=$TOOL_BIN_DIR/../lib
 LIB_DIR=/opt/cloudera/parcels/CDH/lib
 
-export CLASSPATH=$LIB_DIR/hbase/lib/*:$LIB_DIR/hadoop/client/*:/opt/cloudera/parcels/CDH/jars/metrics-core-2.2.0.jar:$LIB_DIR/hadoop/client-0.20/netty-all.jar:/etc/hbase/conf/:$TOOL_LIB_DIR/region-tool-0.1-hbase1.2-cdh5.8.4.jar8.4.jar
+export CLASSPATH=$LIB_DIR/hbase/lib/*:$LIB_DIR/hadoop/client/*:/opt/cloudera/parcels/CDH/jars/metrics-core-2.2.0.jar:$LIB_DIR/hadoop/client-0.20/netty-all.jar:/etc/hbase/conf/:$TOOL_LIB_DIR/region-tool-0.1-hbase1.2-cdh5.8.4.jar
 
 $JAVA_HOME/bin/java -Dlog4j.configuration=tool-log4j.properties admin.RegionsTool "$@"
