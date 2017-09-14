ea8769866610e1bdb54d66d9919e7768d940dbaa
rarana <rafael.arana@cloudera.com>
2017-09-14 14:31:08 +0200

    Improved package procedure

--- bin/region-tool.sh
+++ bin/region-tool.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
+#  Update to your current JAVA HOME
 JAVA_HOME=/usr/java/jdk1.7.0_67-cloudera
 
-TOOL_BIN_DIR=/root/region-tool/bin
-TOOL_LIB_DIR=$TOOL_BIN_DIR/../lib
+# Update to the Region Tool home folder
+TOOL_HOME_DIR=/opt/HBase-RegionTool
+
+TOOL_BIN_DIR=$TOOL_HOME_DIR/bin
+TOOL_LIB_DIR=$TOOL_HOME_DIR/lib
+
 LIB_DIR=/opt/cloudera/parcels/CDH/lib
 
 export CLASSPATH=$LIB_DIR/hbase/lib/*:$LIB_DIR/hadoop/client/*:/opt/cloudera/parcels/CDH/jars/metrics-core-2.2.0.jar:$LIB_DIR/hadoop/client-0.20/netty-all.jar:/etc/hbase/conf/:$TOOL_LIB_DIR/region-tool-0.1-hbase1.2-cdh5.8.4.jar
